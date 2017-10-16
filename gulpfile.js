bbf25fe5cb44eafe497e07d8475da3088e245fcf
Voltokk <vladlexxx@yahoo.com>
2017-10-16 13:42:20 +0300

    Add files via upload
    Babel and SFTP support added .

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,96 @@
 "use strict";
 
 /*Gulp variables*/
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-var cache = require('gulp-cache');
-var csscomb = require('gulp-csscomb');
-var clean = require('gulp-clean');
-var concat = require('gulp-concat');
-var cssnano = require('gulp-cssnano');
-var ftp = require('vinyl-ftp');
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var imagemin = require('gulp-imagemin');
-var notify = require('gulp-notify');
-var plumber = require('gulp-plumber');
-var pngquant = require('imagemin-pngquant');
-var rename = require('gulp-rename');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var uglify = require('gulp-uglifyjs');
-var uncss = require('gulp-uncss');
-var wiredep = require("wiredep").stream;
+const autoprefixer  = require('gulp-autoprefixer');
+const babel         = require('gulp-babel');
+const browserSync   = require('browser-sync');
+const cache         = require('gulp-cache');
+const csscomb       = require('gulp-csscomb');
+const clean         = require('gulp-clean');
+const concat        = require('gulp-concat');
+const cssnano       = require('gulp-cssnano');
+const ftp           = require('vinyl-ftp');
+const gulp          = require('gulp');
+const gutil         = require('gulp-util');
+const imagemin      = require('gulp-imagemin');
+const notify        = require('gulp-notify');
+const plumber       = require('gulp-plumber');
+const pngquant      = require('imagemin-pngquant');
+const rename        = require('gulp-rename');
+const sass          = require('gulp-sass');
+const sftp          = require('gulp-sftp');
+const sourcemaps    = require('gulp-sourcemaps');
+const uglify        = require('gulp-uglifyjs');
+const uncss         = require('gulp-uncss');
+const wiredep       = require('wiredep').stream;
 
 
 /*Path variables*/
-var html_Src    = '';
-var css_Src     = 'assets/css/';
-var sass_Src    = 'assets/sass/';
-var font_Src    = 'assets/fonts/';
-var js_Src      = 'assets/js/';
-var img_Src     = 'images/';
-var libs_Src    = 'libs/';
+const path = {
+    html    : '',
+    css     : 'assets/css/',
+    sass    : 'assets/sass/',
+    fonts   : 'assets/fonts/',
+    js      : 'assets/js/',
+    es6     : 'assets/es6/',
+    images  : 'images/',
+    libs    : 'libs/'
+};
 
 
 /*Browser Sync variables*/
-var bs_index = 'index.html';
+let bs_index = 'index.html';
 
 
 /*FTP connection variables*/
-var site_name       = 'sitename';
-var host_ftp        = 'host';
-var user_ftp        = 'anonymous';
-var pass_ftp        = 'anonymous@';
-var port_ftp        = '21';
-var parallel        = 10;
-var isSFTP          = false;
-var remote_folder   = '/public_html';
+const config = {
+    site            : 'sitename',
+    host            : 'host',
+    user            : 'anonymous',
+    pass            : 'anonymous@',
+    port            : '21',
+    key_path        : null,
+    passphrase      : null,
+    parallel        : 10,
+    remote_folder   : '/public_html'
+};
 
-var globs = [
+const globs = [
+    // 'app/' + path.html + '*.html',
+    'app/' + path.css + '*'
+    // 'app/' + path.js + '*.js',
+    // 'app/' + path.fonts + '**',
+    // 'app/' + path.libs + '**',
     // 'app/**',
-    // 'app/' + js_Src + '*.js',
-    // 'app/' + font_Src + '**',
-    // 'app/' + libs_Src + '**',
-    'app/' + html_Src + '*.html',
-    'app/' + css_Src + '*'
 ];
 
 function getFtpConnection() {
     return ftp.create({
-        host:       host_ftp,
-        user:       user_ftp,
-        password:   pass_ftp,
-        port:       port_ftp,
-        secure:     isSFTP,
-        parallel:   parallel,
+        host:       config.host,
+        user:       config.user,
+        password:   config.pass,
+        port:       config.port,
+        parallel:   config.parallel,
         log:        gutil.log
     });
 }
-
+function getSftpConnection() {
+    return sftp({
+        host:           config.host,
+        user:           config.user,
+        pass:           config.pass,
+        port:           config.port,
+        key:            config.key_path,
+        passphrase:     config.passphrase,
+        remotePath:     config.remote_folder
+    });
+}
 
 /*Error notify settings*/
-var notifyInfo = {
+let notifyInfo = {
     title: 'Gulp'
 };
-var plumberErrorHandler = {
+let plumberErrorHandler = {
     errorHandler: notify.onError({
         title: notifyInfo.title,
         icon: notifyInfo.icon,
@@ -94,64 +111,68 @@ gulp.task('clear-cache', function () {
 
 /*Convert css to scss*/
 gulp.task('convert-css-scss', function () {
-    return gulp.src('app/' + css_Src + '*.css')
-        .pipe(rename({extname: '.conv.scss'}))
-        .pipe(gulp.dest('app/' + sass_Src));
+    return gulp.src('app/' + path.css + '*.css')
+        .pipe(gulp.dest('app/' + path.sass));
 });
 
 /*Enable sass support + auto prefixer and source maps*/
 gulp.task('sass', function () {
-    return gulp.src('app/' + sass_Src + 'style.scss')
+    return gulp.src('app/' + path.sass + '*.scss')
         .pipe(plumber(plumberErrorHandler))
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {cascade: true}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('app/' + css_Src))
-        .pipe(browserSync.reload({stream: true}));
+        .pipe(gulp.dest('app/' + path.css))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+/*Translate ES6 to ES5 via Babel*/
+gulp.task('babel', function () {
+    gulp.src('app/' + path.es6 + '/**/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('app/' + path.js))
+        .pipe(browserSync.reload({stream:true}));
 });
 
 /*Enable bower dependencies*/
-gulp.task('wiredep', function() {
-    gulp.src('app/*.html')
+gulp.task('wiredep', function () {
+    gulp.src('app/' + path.html + '*.html')
         .pipe(wiredep({
-            directory: 'app/' + libs_Src
+            directory: 'app/' + path.libs
         }))
-        .pipe(gulp.dest('app/'));
+        .pipe(gulp.dest('app/' + path.html));
 });
 
 /*Minify and combine script files*/
-gulp.task('script:minify', function () {
-    var js = gulp.src('app/' + js_Src + '**/*')
+gulp.task('script:minify', ['babel'], function () {
+    return gulp.src('app/' + path.js + '**/*.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('app/' + js_Src));
-    var libs = gulp.src('app/' + libs_Src + '**/*.js')
-        .pipe(concat('libs.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('app/' + js_Src));
+        .pipe(gulp.dest('app/' + path.js));
 });
 
 /*Minify styles and lib styles*/
 gulp.task('css:minify', ['sass'], function () {
-    var css = gulp.src('app/' + css_Src + '*.css')
+    return gulp.src('app/' + path.css + '**/*.css')
         .pipe(cssnano())
         .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('dist/' + css_Src));
-    var libs = gulp.src('app/' + libs_Src + '**/*.css')
-        .pipe(cssnano())
-        .pipe(concat('libs.min.css'))
-        .pipe(gulp.dest('dist/' + css_Src));
+        .pipe(gulp.dest('dist/' + path.css));
 });
 
 /*Clean styles from unused selectors*/
 gulp.task('uncss', ['sass'], function () {
-    return gulp.src('app/' + css_Src + '*.css')
+    return gulp.src('app/' + path.css + '*.css')
         .pipe(uncss({
-            html: ['app/' + html_Src + '*.html']
+            html: ['app/' + path.html + '*.html']
         }))
         .pipe(rename({suffix: '.un'}))
-        .pipe(gulp.dest('dist/' + css_Src));
+        .pipe(gulp.dest('dist/' + path.css));
 });
 
 /*Auto reload browser*/
@@ -170,69 +191,90 @@ gulp.task('browser-sync', function () {
 
 /*Minify images*/
 gulp.task('img:minify', function () {
-    return gulp.src('app/' + img_Src + '**/*')
+    return gulp.src('app/' + path.images + '**/*')
         .pipe(cache(imagemin({
             interlaced: true,
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         })))
-        .pipe(gulp.dest('dist/' + img_Src));
+        .pipe(gulp.dest('dist/' + path.images));
 });
 
-/*Ftp deploy*/
-gulp.task('ftp-deploy', function () {
-    var init_connect = getFtpConnection();
 
-    return gulp.src('dist/**', {base: './dist/', buffer: false})
-        .pipe(init_connect.newer(remote_folder))
-        .pipe(init_connect.dest(remote_folder));
+gulp.task('ftp', function () {
+    const init_connect = getFtpConnection();
+    return gulp.src(globs, {base: './app/', buffer: false})
+        .pipe(init_connect.newer(config.remote_folder))
+        .pipe(init_connect.dest(config.remote_folder));
+});
+
+gulp.task('sftp', function () {
+    const init_connect = getSftpConnection();
+    return gulp.src(globs, {base: './app', buffer: false})
+        .pipe(init_connect);
 });
 
 /*Enable watcher for remote server ftp*/
-gulp.task('watch:remote', function() {
+gulp.task('watch:remote:ftp', function () {
     browserSync({
         open: 'external',
-        proxy: site_name,
+        proxy: config.site,
         reloadDelay: 3500,
         notify: false
     });
 
-    var init_connect = getFtpConnection();
+    gulp.watch('app/' + path.sass + '*.scss', ['sass']);
+    gulp.watch('app/' + path.es6 + '**/*.js', ['babel']);
+    gulp.watch(globs, ['ftp']).on('change', browserSync.reload);
+});
 
-    gulp.watch('app/' + sass_Src + '*.scss', ['sass']);
-    gulp.watch(globs, browserSync.reload)
-        .on('change', function(event) {
-            return gulp.src([event.path], {base: './app/', buffer: false})
-                .pipe(init_connect.newer(remote_folder))
-                .pipe(init_connect.dest(remote_folder));
-        });
-    gulp.watch('bower.json', ['wiredep']);
+/*Enable watcher for remote server sftp*/
+gulp.task('watch:remote:sftp', function () {
+    browserSync({
+        open: 'external',
+        proxy: config.site,
+        reloadDelay: 3500,
+        notify: false
+    });
+
+    gulp.watch('app/' + path.sass + '*.scss', ['sass']);
+    gulp.watch('app/' + path.es6 + '**/*.js', ['babel']);
+    gulp.watch(globs, ['sftp']).on('change', browserSync.reload);
 });
 
 /*Enable local watcher*/
 gulp.task('watch:local', ['browser-sync'], function () {
-    gulp.watch('app/' + sass_Src + '*.scss', ['sass']);
-    gulp.watch('app/' + html_Src + '*.html', browserSync.reload);
-    gulp.watch('app/' + js_Src + '**/*.js', browserSync.reload);
+    gulp.watch('app/' + path.html + '*.html', browserSync.reload);
+    gulp.watch('app/' + path.sass + '**/*.scss', ['sass']);
+    gulp.watch('app/' + path.es6 + '**/*.js', ['babel']);
     gulp.watch('bower.json', ['wiredep'], browserSync.reload);
 });
 
 /*Build project for production*/
-gulp.task('build', ['clean', 'img:minify', 'sass', 'css:minify', 'script:minify'], function () {
+gulp.task('build', ['clean', 'img:minify', 'sass', 'css:minify', 'babel', 'script:minify'], function () {
 
-    var buildCss = gulp.src('app/' + css_Src + '*')
-        .pipe(gulp.dest('dist/' + css_Src ));
+    let buildCss = gulp.src('app/' + path.css + '*')
+        .pipe(gulp.dest('dist/' + path.css ));
 
-    var buildLibs = gulp.src('app/' + libs_Src + '**/*')
-        .pipe(gulp.dest('dist/' + libs_Src));
+    let buildLibs = gulp.src('app/' + path.libs + '**/*')
+        .pipe(gulp.dest('dist/' + path.libs));
 
-    var buildFonts = gulp.src('app/' + font_Src + '**/*')
-        .pipe(gulp.dest('dist/' + font_Src));
+    let buildFonts = gulp.src('app/' + path.fonts + '**/*')
+        .pipe(gulp.dest('dist/' + path.fonts));
 
-    var buildJs = gulp.src('app/' + js_Src + '**/*')
-        .pipe(gulp.dest('dist/' + js_Src));
+    let buildJs = gulp.src('app/' + path.js + '**/*')
+        .pipe(gulp.dest('dist/' + path.js));
 
-    var buildHtml = gulp.src('app/' + html_Src + '*.html')
-        .pipe(gulp.dest('dist/' + html_Src));
+    let buildHtml = gulp.src('app/' + path.html + '*.html')
+        .pipe(gulp.dest('dist/' + path.html));
+});
+
+/*Ftp deploy*/
+gulp.task('ftp-deploy', function () {
+    const init_connect = getFtpConnection();
+
+    return gulp.src('dist/**', {base: './dist/', buffer: false})
+        .pipe(init_connect.newer(config.remote_folder))
+        .pipe(init_connect.dest(config.remote_folder));
 });
